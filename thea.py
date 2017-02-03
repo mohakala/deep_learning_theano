2a5e6a614f82ba75bafa2656764a6dd53c8be296
Mikko O. Hakala <mohakala@c308.(none)>
2017-02-03 23:55:21 +0200

    more tests

--- thea.py
+++ thea.py
@@ -4,8 +4,9 @@ import theano.tensor as T
 from theano import function
 from theano import shared
 
-# http://www.analyticsvidhya.com/blog/2016/04/neural-networkds-python-theano/  # THEANO_FLAGS='device=cpu,floatX=float32'    
-
+# http://www.analyticsvidhya.com/blog/2016/04/neural-networkds-python-theano/  # export THEANO_FLAGS='device=cpu,floatX=float32'    
+# export OMP_NUM_THREADS=4
+# srun -n 1 -p gpu --gres=gpu:1 -t 04:00:00 --mem-per-cpu=4G --pty $SHELL
 
 # Variables
 a = T.scalar('a')
@@ -13,46 +14,44 @@ b = T.scalar('b')
 avec = T.vector('av')
 bvec = T.vector('bv')
 
-# Shared variables == Constants
-sh = shared(0.33)
 
 
 # Scalar expression
 c=a*b
 f=function([a,b],c)
 
-# Vector expression
-cvec=avec*bvec*sh  # element-wise multiplication x sh
-fvec=function([avec,bvec],cvec)
-
-
-# Evaluate expression
 inp=[1.5,3]
 output=f(inp[0],inp[1])
 print('Output:',output)
 
+# Shared variables == Constants
+sh = shared(0.33)
 
-# Evaluate vector expresssion
+# Vector expression
+cvec=avec*bvec*sh  # element-wise multiplication x sh
+fvec=function([avec,bvec],cvec)
 in1=[1,2]
 in2=[0,10]
 output2=fvec(in1,in2)
 print('Output2:',output2)
 
+# Expression
+x = T.iscalar('x')
+sh = shared(0)
+f=function([x],sh**2, updates=[(sh,sh+x)])
+
+input=1
+for i in range(3):
+    print('x=1: output3, sh:',f(input),sh.get_value())
 
 
-
-
-
-
-
-
-
-
-assert True==False, "stop" 
 
 
 
 print('Done')
+assert False, "stop" 
+
+
 
 
 
