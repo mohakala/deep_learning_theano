a96ab520ec7a2c74984f8b5df173f7a6a1f75996
mikko.o.hakala@gmail.com <mikko.o.hakala@gmail.com>
2017-02-11 21:32:07 +0200

    added links

--- mnist_test.py
+++ mnist_test.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sat Feb 11 21:15:04 2017
+
+https://lasagne.readthedocs.io/en/latest/user/tutorial.html
+https://github.com/Lasagne/Lasagne/blob/master/examples/mnist.py
 """
 
 from __future__ import print_function
