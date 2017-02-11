b66401433ae6d66fd7c13546d045b369b8171b13
mikko.o.hakala@gmail.com <mikko.o.hakala@gmail.com>
2017-02-11 21:26:56 +0200

    started the mnist example

---
+++ mnist_test.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 11 21:15:04 2017
+"""
+
+from __future__ import print_function
+
+import sys
+import os
+import time
+
+import numpy as np
+import theano
+import theano.tensor as T
+
+import lasagne
+
+
+
+
+
+
+#assert False, "-- FORCED STOP --"
+
+print('Done')
+
